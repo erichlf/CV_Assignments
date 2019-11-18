cd01c372cd76be9cc121c7bbf2ac0fe9223b131e
Erich L Foster <efoster@carnegierobotics.com>
2019-11-18 08:41:21 -0500

    Split Pojection Functions

--- distortion.cpp
+++ distortion.cpp
@@ -2,24 +2,19 @@
 
 int main()
 {
-  cv::Mat_<double> fisheye_model(1, 4);
-  fisheye_model << 0.1, -0.2, 0.03, 0.001;
+  cv::Matx<double,1, 4> fisheye_model(0.1, -0.2, 0.03, 0.001);
 
-  cv::Mat_<double> plumbbob_model(1, 5);
-  plumbbob_model << 0.2, 0.1, 0.001, 0.002, -0.003;
+  cv::Matx<double, 1, 5> plumbbob_model(0.2, 0.1, 0.001, 0.002, -0.003);
 
-  cv::Mat_<double> rational_poly_model(1, 8);
-  rational_poly_model << 0.4, -0.3, 0.001, 0.004, 0.1, 0.2, -0.12, 0.3;
+  cv::Matx<double, 1, 8> rational_poly_model(0.4, -0.3, 0.001, 0.004, 0.1, 0.2, -0.12, 0.3);
 
-  cv::Mat_<double> camera_matrix(3, 3);
-  camera_matrix << 584.4, 0, 622.8,
-                   0, 584.4, 538.3,
-                   0, 0, 1;
+  cv::Matx<double, 3, 3> camera_matrix(584.4, 0, 622.8,
+                                       0, 584.4, 538.3,
+                                       0, 0, 1);
 
-  cv::Mat_<double> world_points(3, 3);
-  world_points << 5, 10, 15,
-                  0, 0, 10,
-                  1.23, 3.4, 5.67;
+  cv::Matx<double, 3, 3> world_points(5, 10, 15,
+                                      0, 0, 10,
+                                      1.23, 3.4, 5.67);
   /*
   std::vector<cv::Point3d> world_points_vector{
     {world_points.at<double>(0, 0), world_points.at<double>(0, 1), world_points.at<double>(0, 2)},
@@ -27,11 +22,9 @@ int main()
     {world_points.at<double>(2, 0), world_points.at<double>(2, 1), world_points.at<double>(2, 2)}};
     */
 
-  cv::Mat_<double> rvec(1, 3);
-  rvec << 0, 0, 0;
+  cv::Vec3d rvec(0, 0, 0);
 
-  cv::Mat_<double> tvec(1, 3);
-  tvec << 0, 0, 0;
+  cv::Vec3d tvec(0, 0, 0);
 
   std::vector<cv::Point2d> plumbbob_image;
   assignments::project_points(world_points, rvec, tvec, camera_matrix, plumbbob_model, plumbbob_image);
--- include/utilities.hpp
+++ include/utilities.hpp
@@ -5,74 +5,160 @@
 namespace assignments
 {
 
-void project_points(const cv::Mat& world_points, const cv::Mat& rvec, const cv::Mat& tvec, const cv::Mat& K,
-                    const cv::Mat& dist_coeffs, std::vector<cv::Point2d>& image_points)
+/*
+ * \brief projects the distorted and z-normalized world point into image points
+ * \param distorted_point distorted and z-normalized world point
+ * \param K camera matrix
+ */
+cv::Point2d get_image_point_(cv::Point2d distorted_point, const cv::Matx<double, 3, 3> K)
 {
-  double k1 = 0., k2 = 0., k3 = 0., p1 = 0., p2 = 0., k4 = 0., k5 = 0., k6 = 0.;
-  if (dist_coeffs.cols >= 4)  // fisheye model
-  {
-    k1 = dist_coeffs.at<double>(0, 0);
-    k2 = dist_coeffs.at<double>(0, 1);
-    k3 = dist_coeffs.at<double>(0, 2);
-    k4 = dist_coeffs.at<double>(0, 3);
-  }
+  const auto fx = K(0, 0);
+  const auto fy = K(1, 1);
+  const auto cx = K(0, 2);
+  const auto cy = K(1, 2);
 
-  if (dist_coeffs.cols >= 5)  // plumb bob model
-  {
-    p1 = dist_coeffs.at<double>(0, 2);
-    p2 = dist_coeffs.at<double>(0, 3);
-    k3 = dist_coeffs.at<double>(0, 4);
-    k4 = dist_coeffs.at<double>(0, 5);
-  }
+  const auto u = distorted_point.x * fx + cx;
+  const auto v = distorted_point.y * fy + cy;
 
-  if (dist_coeffs.cols == 8) // rational polynomial
-  {
-    k5 = dist_coeffs.at<double>(0, 6);
-    k6 = dist_coeffs.at<double>(0, 7);
-  }
+  return {u, v};
+}
 
-  double fx = K.at<double>(0, 0);
-  double fy = K.at<double>(1, 1);
-  double cx = K.at<double>(0, 2);
-  double cy = K.at<double>(1, 2);
+/*
+ * \brief projects 3D world point into image points with fisheye distortion
+ * \param world_points  3D world point data
+ * \param rvec  rotation vector
+ * \param tvec  translation vector
+ * \param K camera matrix
+ * \param dist_coefficients distortion coefficients k1, k2, k3, k4
+ * \param image_points  points in image after distortion
+ */
+void project_points(const cv::InputArray& world_points_, const cv::Vec3d& rvec, const cv::Vec3d& tvec,
+                    const cv::Matx<double, 3, 3>& K, const cv::Matx<double, 1, 4>& dist_coeffs,
+                    std::vector<cv::Point2d>& image_points)
+{
+  const auto world_points = world_points_.getMat();
+
+  const auto k1 = dist_coeffs(0, 0);
+  const auto k2 = dist_coeffs(0, 1);
+  const auto k3 = dist_coeffs(0, 2);
+  const auto k4 = dist_coeffs(0, 3);
 
   for (int i = 0; i < world_points.rows; ++i)
   {
-    double x = world_points.at<double>(i, 0);
-    double y = world_points.at<double>(i, 1);
-    double z = world_points.at<double>(i, 2);
+    const auto x = world_points.at<double>(i, 0);
+    const auto y = world_points.at<double>(i, 1);
+    const auto z = world_points.at<double>(i, 2);
 
-    double xp = x / z;
-    double yp = y / z;
+    const auto xp = x / z;
+    const auto yp = y / z;
+
+    const auto r = sqrt(xp * xp + yp * yp);
+    const auto theta = atan(r);
+
+    const double theta_r = r != 0 ?
+                           theta * (1 + k1 * theta * theta + k2 * pow(theta, 4) + k3 * pow(theta, 6) + k4 * pow(theta, 8)) / r
+                           : 0;
+
+    const double xpp = theta_r * xp;
+    const double ypp = theta_r * yp;
+
+    const auto image_point = get_image_point_({xpp, ypp}, K);
+    image_points.push_back(image_point);
+  }
+}
+
+/*
+ * \brief projects 3D world point into image points with plumb bob distortion
+ * \param world_points  3D world point data
+ * \param rvec  rotation vector
+ * \param tvec  translation vector
+ * \param K camera matrix
+ * \param dist_coefficients distortion coefficients k1, k2, p1, p2, k3, k4
+ * \param image_points  points in image after distortion
+ */
+void project_points(const cv::InputArray& world_points_, const cv::Vec3d& rvec, const cv::Vec3d& tvec,
+                    const cv::Matx<double, 3, 3>& K, const cv::Matx<double , 1, 5>& dist_coeffs,
+                    std::vector<cv::Point2d>& image_points)
+{
+  const auto world_points = world_points_.getMat();
+  const auto k1 = dist_coeffs(0, 0);
+  const auto k2 = dist_coeffs(0, 1);
+  const auto p1 = dist_coeffs(0, 2);
+  const auto p2 = dist_coeffs(0, 3);
+  const auto k3 = dist_coeffs(0, 4);
+
+  for (int i = 0; i < world_points.rows; ++i)
+  {
+    const auto x = world_points.at<double>(i, 0);
+    const auto y = world_points.at<double>(i, 1);
+    const auto z = world_points.at<double>(i, 2);
+
+    const double xp = x / z;
+    const double yp = y / z;
+
+    const auto r = sqrt(xp * xp + yp * yp);
+
+    const auto theta = (1. + k1 * r * r + k2 * pow(r, 4) + k3 * pow(r, 6));
+
+    const double dx1 = 2. * p1 * xp * yp + p2 * (r * r + 2. * xp * xp);
+    const double dx2 = p1 * (r * r + 2. * yp * yp) + 2. * p2 * xp * yp;
+
+
+    const double xpp = theta * xp + dx1;
+    const double ypp = theta * yp + dx2;
+
+    const auto image_point = get_image_point_({xpp, ypp}, K);
+    image_points.push_back(image_point);
+  }
+}
+
+/*
+ * \brief projects 3D world point into image points with rational polynomial distortion
+ * \param world_points  3D world point data
+ * \param rvec  rotation vector
+ * \param tvec  translation vector
+ * \param K camera matrix
+ * \param dist_coefficients distortion coefficients k1, k2, p1, p2, k3, k4, k5, k6
+ * \param image_points  points in image after distortion
+ */
+void project_points(const cv::InputArray& world_points_, const cv::Vec3d& rvec, const cv::Vec3d& tvec,
+                    const cv::Matx<double, 3, 3>& K, const cv::Matx<double , 1, 8>& dist_coeffs,
+                    std::vector<cv::Point2d>& image_points)
+{
+  const auto world_points = world_points_.getMat();
+
+  const auto k1 = dist_coeffs(0, 0);
+  const auto k2 = dist_coeffs(0, 1);
+  const auto p1 = dist_coeffs(0, 2);
+  const auto p2 = dist_coeffs(0, 3);
+  const auto k3 = dist_coeffs(0, 4);
+  const auto k4 = dist_coeffs(0, 5);
+  const auto k5 = dist_coeffs(0, 6);
+  const auto k6 = dist_coeffs(0, 7);
+
+  for (int i = 0; i < world_points.rows; ++i)
+  {
+    const auto x = world_points.at<double>(i, 0);
+    const auto y = world_points.at<double>(i, 1);
+    const auto z = world_points.at<double>(i, 2);
+
+    const double xp = x / z;
+    const double yp = y / z;
 
     double r = sqrt(xp * xp + yp * yp);
-    double theta;
-    double dx1 = 0, dx2 = 0;
 
-    if (dist_coeffs.cols == 4)
-    {
-      theta = atan(r);
-      theta = r != 0 ?
-              theta * (1 + k1 * theta * theta + k2 * pow(theta, 4) + k3 * pow(theta, 6) + k4 * pow(theta, 8)) / r :
-              0;
-    }
-    else
-    {
+    const double theta = (1. + k1 * r * r + k2 * pow(r, 4) + k3 * pow(r, 6)) /
+                         (1. + k4 * r * r + k5 * pow(r, 4) + k6 * pow(r, 6));
 
-      theta = (1. + k1 * r * r + k2 * pow(r, 4) + k3 * pow(r, 6)) /
-              (1. + k4 * r * r + k5 * pow(r, 4) + k6 * pow(r, 6));
-      dx1 = 2. * p1 * xp * yp + p2 * (r * r + 2. * xp * xp);  // zero for fisheye
-      dx2 = p1 * (r * r + 2. * yp * yp) + 2. * p2 * xp * yp;  // zero for fisheye
-    }
+    const double dx1 = 2. * p1 * xp * yp + p2 * (r * r + 2. * xp * xp);
+    const double dx2 = p1 * (r * r + 2. * yp * yp) + 2. * p2 * xp * yp;
 
 
     double xpp = theta * xp + dx1;
     double ypp = theta * yp + dx2;
 
-    double u = fx * xpp + cx;
-    double v = fy * ypp + cy;
-
-    image_points.push_back({u, v});
+    const auto image_point = get_image_point_({xpp, ypp}, K);
+    image_points.push_back(image_point);
   }
 }
 

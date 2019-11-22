2c97a8474037b7bcb5621b09a9d6fa57f5f36631
Erich L Foster <efoster@carnegierobotics.com>
2019-11-22 07:59:42 -0500

    Add Rays from Camera to Points

--- pose.cpp
+++ pose.cpp
@@ -31,7 +31,7 @@ int main()
   const auto reprojection_error = assignments::reprojection_error(object_points, distorted_image_points, rvec, tvec,
                                                                   camera_matrix, fisheye_model);
 
-  cv::Affine3f objects_from_camera = cv::Affine3f(rvec, tvec).inv();
+  const auto objects_from_camera = cv::Affine3f(rvec, tvec).inv();
 
   std::cout << "Rotation:" << std::endl;
   std::cout << objects_from_camera.rvec() << std::endl;
@@ -41,16 +41,31 @@ int main()
   std::cout << reprojection_error << std::endl;
 
 
+  cv::viz::Viz3d window = cv::viz::Viz3d("Camera Pose");
+  window.showWidget("Axis", cv::viz::WCoordinateSystem());
+  window.showWidget("Camera", cv::viz::WCameraPosition(camera_matrix));
+
   cv::Mat object_points_C3(object_points.rows, 1, CV_64FC3);
   for (int i = 0; i < object_points.rows; ++i)
-    object_points_C3.at<cv::Vec3d>(i) = cv::Point3d{object_points(i, 0), object_points(i, 1), object_points(i, 2)};
+  {
+    const auto object_point = cv::Point3d{object_points(i, 0), object_points(i, 1), object_points(i, 2)};
+    object_points_C3.at<cv::Vec3d>(i) = object_point;
 
-  cv::viz::Viz3d window = cv::viz::Viz3d("Camera Pose");
-  // window.showWidget("Axis", cv::viz::WCoordinateSystem());
-  window.showWidget("Camera", cv::viz::WCameraPosition(camera_matrix));
-  window.showWidget("Points", cv::viz::WCloud(object_points_C3, cv::viz::Color::green()));
+    window.showWidget("L" + std::to_string(i), cv::viz::WLine({objects_from_camera.translation()(0),
+                                                               objects_from_camera.translation()(1),
+                                                               objects_from_camera.translation()(2)},
+                                                              object_point));
+
+    window.showWidget("P" + std::to_string(i), cv::viz::WText3D("P" + std::to_string(i), object_point, 0.25));
+  }
+
+  window.showWidget("Points", cv::viz::WCloud(object_points_C3, cv::viz::Color::red()));
 
   window.setWidgetPose("Camera", objects_from_camera);
+  window.setWidgetPose("Axis", objects_from_camera);
+
+  window.setWindowSize({400, 400});
+  window.resetCamera();  // makes image proportional to the current window size
 
   while (!window.wasStopped())
     window.spinOnce(1, true);
